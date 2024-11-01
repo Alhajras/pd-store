c751a4a964a8d4df8f20e6d085d6458c2e629162
Alhajras Algdairy <alhajras.algdairy@fifty2.eu>
2024-11-01 12:33:03 +0100

    Delete secret file

--- src/environments/secret.ts
+++
@@ -1,8 +0,0 @@
-export const  FIREBASE = {
-  apiKey: "AIzaSyAo206tHJEmiSPxxAMxP5-_72IUw5F3TPo",
-  authDomain: "pixie-dus.firebaseapp.com",
-  projectId: "pixie-dus",
-  storageBucket: "pixie-dus.firebasestorage.app",
-  messagingSenderId: "345783407172",
-  appId: "1:345783407172:web:fa6ecbd252de3e3403a211",
-  measurementId: "G-BEBBECWQ2S"  }
