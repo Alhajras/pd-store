c79fe829a8c0aecf77c481a2a03e9e6f7b6d1de0
Alhajras Algdairy <kharabeash@gmail.com>
2024-12-11 16:05:17 +0100

    Use vrs

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,12 @@
-import {FIREBASE} from "src/secret";
-
 export const environment = {
   production: true,
-  firebase: FIREBASE
+  firebase: {
+    apiKey: "AIzaSyAo206tHJEmiSPxxAMxP5-_72IUw5F3TPo",
+    authDomain: "pixie-dus.firebaseapp.com",
+    projectId: "pixie-dus",
+    storageBucket: "pixie-dus.firebasestorage.app",
+    messagingSenderId: "345783407172",
+    appId: "1:345783407172:web:fa6ecbd252de3e3403a211",
+    measurementId: "G-BEBBECWQ2S"
+  }
 };
